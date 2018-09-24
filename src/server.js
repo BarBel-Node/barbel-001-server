582123a2a09c5d190b7cc216409f46ed85d7ad8f
Firmansyah Adiputra <frm.adiputra@gmail.com>
2018-09-24 20:55:07 +0700

    Generate HTML response

--- src/server.js
+++ src/server.js
@@ -10,8 +10,15 @@ const server = http.createServer((req, res) => {
 	// Setiap respon harus ada status code
 	// Biasanya juga disertakan content type, dan response body
 	res.statusCode = 200 // HTTP status code 200 : success
-	res.setHeader('Content-Type', 'text/plain') // content type berupa teks
-	res.end('Hello World\n') // response body
+	
+	// respon dalam HTML
+	res.setHeader('Content-Type', 'text/html') // content type berupa teks
+	res.write('<html>')
+	res.write('<body>')
+	res.write('<p style="color:red;">Hello world!</p>')
+	res.write('</body>')
+	res.write('</html>')
+	res.end()
 })
 
 // Jalankan server
